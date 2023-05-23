b3b5f847f12f725809d05dc339442f63184b46be
heshameldawy <Heshameldawy__@outlook.com>
2023-05-23 01:39:32 -0400

    building inputs & buttons components

--- src/components/header/MainHeader.tsx
+++ src/components/header/MainHeader.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-
+import './navStyling.css';
 const MainHeader = () => {
     return (
         <header>
-
-        </header>
+            <nav className='nav collasible collasible--expanded'>
+                <a href="/"><img src="logo.png" height={90} width={150} alt="" /></a>
+                <svg className='icon icon--white nav__toggler'>
+                    <use xlinkHref="sprite.svg#menu"></use>
+                </svg>
+                <ul className='list nav__list collasible__content'>
+                    <li className='nav__item'><a href="#">About me</a></li>
+                    <li className='nav__item'><a href="#">Projects</a></li>
+                    <li className='nav__item'><a href="#">Work Experience</a></li>
+                    <li className='nav__item'><a href="#">Blog</a></li>
+                    <li className='nav__item'><a href="#">Contact Me</a></li>
+                </ul>
+            </nav>
+        </header >
     )
 }
 
